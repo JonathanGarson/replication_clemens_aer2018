ac4cad81ca07eb4ae4aa65677dbddbe8bf9be84e
JonathanGarson <garsonjonathan22@gmail.com>
2024-09-23 23:43:59 +0200

    ready to start working

---
+++ code/01_generate/0_convert_to_parquet.R
@@ -0,0 +1,14 @@
+# This code converts to parquet formats all .dta dataset
+
+rm(list= ls())
+gc()
+
+library(arrow)
+library(haven)
+library(glue)
+
+for (file in list.files("original_data/", pattern = ".dta")){
+  data = read_dta(glue("original_data/{file}"))
+  data_name = gsub(".dta", "", x = file)
+  write_parquet(data, glue("data/raw/{data_name}.parquet"))
+}
