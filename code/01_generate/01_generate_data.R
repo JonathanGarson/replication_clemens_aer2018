228e877f12af51febd15566e324201b113f26bab
JonathanGarson <garsonjonathan22@gmail.com>
2024-09-28 15:30:30 +0200

    updated renv

---
+++ code/01_generate/01_generate_data.R
@@ -0,0 +1,6 @@
+# This code generates the data used by Clemens & al (2018)
+# It reproduces the data tables producede by the code : "bracero_aer_code.do" from line 54 to 
+
+library(arrow)
+library(data.table)
+
